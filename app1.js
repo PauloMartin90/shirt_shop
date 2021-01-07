6be53012871d86b280a462159014c56ec532b57b
Paulo Martin <paumar559@gmail.com>
2021-01-07 14:29:22 -0800

    Updates

--- app1.js
+++ app1.js
@@ -1,3 +1,20 @@
-let username = prompt('Please Enter Your Name');
 
-document.write(userName)
+let variable1 = prompt("What is variable 1?");
+
+
+
+function checker() {
+
+        document.write(variable1)
+}
+
+function whatis() {
+    if (variable1 == "house") {
+        document.write(variable1)
+    } 
+    else if (variable1 != "house" ) {
+        document.write( "You found the change" )
+    }
+
+    
+}
--- app2.js
+++ app2.js
@@ -1,6 +1,15 @@
-  
-if (userName == 'Paulo'){
-    document.write('<h3>' + 'Hiya' userName + '</h3>');
-} else {
-    document.write('<h3>' + 'Welcome Guest' + '</h3>')
+let variable2 = prompt("What is variable 2?");
+
+document.write(varaible2)
+
+function whatmyUser() {
+var name
+    if (name == 1) {
+        name == "Cool Paulo";
+    }
+        
+        else (name == 2); {
+        name == "Correct Answer";
+    }
+return name;
 }
