41c7c618bc993ac82bb876efa3961fe69dfc1933
Paulo Martin <paumar559@gmail.com>
2021-01-07 18:01:03 -0800

    Hurray

--- app1.js
+++ app1.js
@@ -1,5 +1,18 @@
+function variable_1() {
+    let variable1 = prompt("What is variable 1?");
+    document.write(variable1)
+    return variable1
+}
 
-let variable1 = prompt("What is variable 1?");
+
+
+
+function variable_2() {
+    let variable2= prompt("What is variable 2?");
+    document.write(variable2)
+    return variable2    
+}
+let y = variable2
 
 
 
@@ -8,13 +21,25 @@ function checker() {
         document.write(variable1)
 }
 
-function whatis() {
-    if (variable1 == "house") {
-        document.write(variable1)
+function whatis(bob) {
+    if (bob == 1) {
+        document.write(bob);
     } 
-    else if (variable1 != "house" ) {
-        document.write( "You found the change" )
-    }
+    else if (bob != 1 ) {
+        document.write( "YOU ARE WRONG" );
+    }    
+}
 
+function summ() {
+
+    var summm = variable1() + variable2();
+
+    document.write(summm)
+}
+
+
+function confirmation() {
     
+    let confirmation = prompt("pick a number 1-100")
+
 }
